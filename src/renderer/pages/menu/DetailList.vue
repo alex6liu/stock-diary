b94e5765aabda0d1a0cf999f61e4055b43067f61
Alex Liu <alex_liu@epam.com>
2019-03-29 17:35:54 +0800

    change

--- src/renderer/pages/menu/DetailList.vue
+++ src/renderer/pages/menu/DetailList.vue
@@ -2,15 +2,18 @@
   <div>{{code}}</div>
 </template>
 <script>
+import apis from '../../utils/apis.js';
 
 export default {
   data() {
     return {
       code: 0,
+      api: '',
     };
   },
   mounted() {
     this.code = this.$route.params.code;
+    this.api = apis.detail + this.code + '.js';
   },
   computed: {},
   methods: {},
